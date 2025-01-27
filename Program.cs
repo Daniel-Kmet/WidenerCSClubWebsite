7349129324e3c7b96749670d8f20b3b7e6a37c81
Daniel-Kmet <96326478+Daniel-Kmet@users.noreply.github.com>
2025-01-26 23:29:19 -0500

    Updated Home Page and better files

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using WidenerCSCIWebPage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
--- obj/Debug/net8.0/WidenerCSCIWebPage.AssemblyInfo.cs
+++ obj/Debug/net8.0/WidenerCSCIWebPage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WidenerCSCIWebPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41c725adefef0d8e71b7b095801fe4caa85401d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WidenerCSCIWebPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WidenerCSCIWebPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
